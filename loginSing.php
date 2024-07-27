e4e1acf40447911333f189986940b6e824d6bcd6
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2024-07-27 10:19:40 -0600

    Inicio

---
+++ loginSing.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+ 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/login.css">
+</head>
+<body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form>
+        <h3>Registro de usuario</h3>
+
+        <label for="cedula">Cédula</label>
+        <input type="text" placeholder="Cédula" id="cedula">
+
+        <label for="nombre">Nombre</label>
+        <input type="text" placeholder="Nombre" id="nombre">
+
+        <label for="telefono">Telefono</label>
+        <input type="text" placeholder="Teléfono" id="telefono" >
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Contraseña" id="password">
+
+        <button>Registrarse</button>
+
+        
+       
+    </form>
+</body>
+</html>
